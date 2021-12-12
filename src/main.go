75867721055f14106411fdc8c7b9beb94edf4b3e
mohammadshahnawazakhter <shahnawazakhter@live.com>
2021-12-12 12:00:02 +0530

    directory structure refactored. Go module tested and working.

---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shahnawaz1985/hello-world/src/hello"
+	"github.com/Shahnawaz1985/hello-world/src/world"
+)
+
+func main() {
+	fmt.Printf(hello.Greet())
+	fmt.Printf(world.Greet())
+}
