9aaeda0cfa042e348b76764754a560f9953d4d36
mohammadshahnawazakhter <shahnawazakhter@live.com>
2021-12-12 11:41:47 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shahnawaz1985/hello-world
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shahnawaz1985/hello-world/hello"
+	"github.com/Shahnawaz1985/hello-world/world"
+)
+
+func main(){
+	fmt.Println(hello.Greet())
+	fmt.Println(world.Greet())
+}
